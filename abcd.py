8642ec4cbbd1d8ff168cc907cab59805149ee3d3
obich <g.shchet@gmail.com>
2024-04-27 11:59:02 +0300

    base

---
+++ abcd.py
@@ -0,0 +1,28 @@
+import pygame
+import requests
+import os
+import sys
+
+c1, c2, scale = map(float, input().split())
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={c1},{c2}&spn={scale},{scale}&l=sat"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
